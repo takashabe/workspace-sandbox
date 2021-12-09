00a779111ce37d332cbf66e3b05e7434e155432e
takashabe <takashabe.io@gmail.com>
2021-12-09 21:43:13 +0900

    fix

--- workspace/main/go.mod
+++ workspace/main/go.mod
@@ -1,5 +1,3 @@
 module github.com/takashabe/workspace-sandbox/workspace/main
 
 go 1.18
-
-require github.com/takashabe/workspace-sandbox/workspace/shop v0.0.0-20211209053201-28973bd3875a
--- workspace/main/main.go
+++ workspace/main/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/takashabe/workspace-sandbox/workspace/workspaceshop"
 )
 
 func main() {
-	s := &workspaceshop.Shop{
-		Name: "multi",
-	}
-	fmt.Printf("%#v", s)
+	// s := &workspaceshop.Shop{
+	//   Name: "multi",
+	// }
+	fmt.Printf("%#v", "a")
 }
